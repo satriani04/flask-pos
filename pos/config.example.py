5858a28f8bde0b6ba8efc6a0ee9d66871819f3b0
satriani04 <winartosaputro@gmail.com>
2017-06-08 19:41:02 +0700

    create config example

---
+++ pos/config.example.py
@@ -0,0 +1,3 @@
+class Config(object):
+	SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/database'
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
--- pos/config.py
+++
@@ -1,3 +0,0 @@
-class Config(object):
-	SQLALCHEMY_DATABASE_URI = 'mysql://root:winsap5474@localhost/pos'
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
